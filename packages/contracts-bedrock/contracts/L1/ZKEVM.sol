45a1d4c1a265630b1328a1e137b095ad83fafb1a
kukoomomo <kukoomomo@gmail.com>
2023-06-06 14:43:19 +0800

    update zkevm contract and add test

--- packages/contracts-bedrock/contracts/L1/ZKEVM.sol
+++ packages/contracts-bedrock/contracts/L1/ZKEVM.sol
@@ -22,8 +22,8 @@ contract ZKEVM is CircuitConfig,OwnableUpgradeable {
     }
 
     struct BatchChallenge{
-        address challenger;
         uint64 batchIndex;
+        address challenger;
         uint256 challengeDeposit;
         uint256 startTime;
         bool finished;
@@ -35,7 +35,6 @@ contract ZKEVM is CircuitConfig,OwnableUpgradeable {
 
     // Last batch sent by the sequencers
     uint64 public lastBatchSequenced;
-    uint64 public challengeNum;
 
     uint256 public constant PROOF_WINDOW = 100;
     uint256 public constant FINALIZATION_PERIOD_SECONDS = 100000;
@@ -45,11 +44,12 @@ contract ZKEVM is CircuitConfig,OwnableUpgradeable {
     mapping(uint64 => bytes32) public commitments;
     mapping(uint64 => bytes32) public stateRoots;
     mapping(uint64 => uint256) public originTimestamps;
+    mapping(uint64 => bool) public confirmBatchIndex;
     mapping(uint64 => BatchChallenge) public challenges; // batchIndex  => Batch
 
-    event ChallengeState(uint256 indexed challengeIndex, address challenger, uint256 batchIndex, uint256 challengeDeposit);
+    event ChallengeState(uint256 indexed batchIndex, address challenger, uint256 challengeDeposit);
     event SubmitBatches(uint64 indexed numBatch);
-    event ChallengeRes(uint256 indexed challengeIndex, address winner, string res);
+    event ChallengeRes(uint256 indexed batchIndex, address winner, string res);
 
     // todo submitter may change to validators
     modifier onlySubmitter() {
@@ -58,11 +58,27 @@ contract ZKEVM is CircuitConfig,OwnableUpgradeable {
         _;
     }
 
-    function initialize(OptimismPortal _portal, address _submitter) external payable initializer{
+    // todo challenger may change to validators
+    modifier onlyChallenger() {
+        require(challenger != address(0),"Challenger cannot be address(0)");
+        require(msg.sender == challenger, "Caller not challenger");
+        _;
+    }
+
+    constructor(OptimismPortal _portal, address _submitter, address _challenger){
+        initialize(_portal, _submitter, _challenger);
+    }
+
+    function initialize(
+        OptimismPortal _portal,
+        address _submitter,
+        address _challenger
+    ) public payable initializer{
         __Ownable_init();
         lastBatchSequenced = 0;
         PORTAL = _portal;
         submitter = _submitter;
+        challenger = _challenger;
         deposits[submitter] += msg.value;
     }
 
@@ -72,7 +88,7 @@ contract ZKEVM is CircuitConfig,OwnableUpgradeable {
     }
 
     function submitBatches(BatchData[] calldata batches) external onlySubmitter{
-        require(deposits[submitter] > MIN_DEPOSIT);
+        require(deposits[submitter] >= MIN_DEPOSIT,"Insufficient staking amount");
         uint256 batchesNum = batches.length;
         uint64 currentBatchSequenced = lastBatchSequenced;
 
@@ -93,82 +109,100 @@ contract ZKEVM is CircuitConfig,OwnableUpgradeable {
                 hash := keccak256(add(publicInput, 32), mul(mload(publicInput), 32))
             }
 
-            currentBatchSequenced++;
             commitments[currentBatchSequenced] = hash;
             stateRoots[currentBatchSequenced] = batches[i].stateRoot;
             originTimestamps[currentBatchSequenced] = block.timestamp;
+            currentBatchSequenced++;
         }
         lastBatchSequenced = currentBatchSequenced;
 
         emit SubmitBatches(lastBatchSequenced);
     }
 
+    function confirmBatch(uint64 batchIndex) public {
+        require(!isBatchInChallenge(batchIndex));
+        bool insideChallengeWindow =
+            originTimestamps[batchIndex] + FINALIZATION_PERIOD_SECONDS > block.timestamp;
+        require(!insideChallengeWindow, "Cannot confirm batch inside the challenge window");
+        // todo confirm one by one?
+        confirmBatchIndex[batchIndex] = true;
+    }
+
+    // todo : challenge by validator
     // challengeState challenges a batch by submitting a deposit.
-    function challengeState(uint64 batchIndex) external payable {
-        require(challenger == address(0), "Already in challenge");
+    function challengeState(uint64 batchIndex)
+        external
+        payable
+        onlyChallenger
+    {
+        require(originTimestamps[batchIndex] != 0,"Batch not exist");
+        require(challenges[batchIndex].challenger == address(0), "Already has challenge");
         // check challenge window
         // todo get finalization period from output oracle
-        bool insideChallengeWindow = originTimestamps[batchIndex] + FINALIZATION_PERIOD_SECONDS > block.timestamp;
+        bool insideChallengeWindow =
+            originTimestamps[batchIndex] + FINALIZATION_PERIOD_SECONDS > block.timestamp;
         require(insideChallengeWindow, "Cannot challenge batch outside the challenge window");
 
-        challenger = msg.sender;
         // check challenge amount
         require(msg.value >= MIN_DEPOSIT);
         deposits[challenger] += msg.value;
-        uint64 challengeIndex = ++challengeNum;
-        challenges[challengeIndex] = BatchChallenge(msg.sender, batchIndex, msg.value, block.timestamp, false);
-        emit ChallengeState(challengeIndex, msg.sender, batchIndex, msg.value);
+        challenges[batchIndex] =
+            BatchChallenge(batchIndex, msg.sender, msg.value, block.timestamp, false);
+        emit ChallengeState(batchIndex, msg.sender, msg.value);
     }
 
     // proveState proves a batch by submitting a proof.
-    function proveState(uint64 challengeIndex, bytes calldata proof) external{
+    function proveState(uint64 batchIndex, bytes calldata proof) external{
         // check challenge exists
-        require(challenges[challengeIndex].challenger != address(0));
-        require(!challenges[challengeIndex].finished);
-        bool insideChallengeWindow = challenges[challengeIndex].startTime + PROOF_WINDOW > block.timestamp;
-        if (insideChallengeWindow) {
-            _challengerWin(challengeIndex,"timeout");
+        require(challenges[batchIndex].challenger != address(0));
+        require(!challenges[batchIndex].finished, "Challenge already finished");
+        bool insideChallengeWindow =
+            challenges[batchIndex].startTime + PROOF_WINDOW > block.timestamp;
+        if (!insideChallengeWindow) {
+            _challengerWin(batchIndex,"timeout");
             // todo pause PORTAL contracts
             // PORTAL.pause();
         } else {
             // check proof
-            require(proof.length > 0);
-            require(msg.sender == submitter);
-            require(_verifyProof(proof, commitments[challenges[challengeIndex].batchIndex]),"Prove failed");
-            _defenderWin(challengeIndex, "prove success");
+            require(proof.length > 0, "Invalid proof");
+            require(
+                _verifyProof(proof, commitments[batchIndex]),
+                "Prove failed"
+                );
+            _defenderWin(batchIndex, "prove success");
         }
-        challenges[challengeIndex].finished = true;
+        challenges[batchIndex].finished = true;
     }
 
-    function _defenderWin(uint64 challengeIndex, string memory  _type) internal {
-        address challengerAddr = challenges[challengeIndex].challenger;
-        uint256 challengeDeposit = challenges[challengeIndex].challengeDeposit;
+    function _defenderWin(uint64 batchIndex, string memory  _type) internal {
+        address challengerAddr = challenges[batchIndex].challenger;
+        uint256 challengeDeposit = challenges[batchIndex].challengeDeposit;
         uint256 submitterDeposit = deposits[submitter];
         deposits[challengerAddr] -= challengeDeposit;
         deposits[submitter] += challengeDeposit;
-        emit ChallengeRes(challengeIndex, submitter, _type);
+        emit ChallengeRes(batchIndex, submitter, _type);
     }
 
-    function _challengerWin(uint64 challengeIndex, string memory _type) internal {
-        address challengerAddr = challenges[challengeIndex].challenger;
+    function _challengerWin(uint64 batchIndex, string memory _type) internal {
+        address challengerAddr = challenges[batchIndex].challenger;
         uint256 submitterDeposit = deposits[submitter];
         deposits[submitter] -= MIN_DEPOSIT;
         deposits[challengerAddr] += MIN_DEPOSIT;
-        emit ChallengeRes(challengeIndex, challengerAddr, _type);
+        emit ChallengeRes(batchIndex, challengerAddr, _type);
     }
 
     function withdraw(uint256 amount) public {
-        require(!isInChallenge(msg.sender));
+        require(!isUserInChallenge(msg.sender),"User is in challenge");
         uint256 value = deposits[msg.sender];
         require(amount > 0);
         uint256 withdrawAmount = amount;
         if (amount + MIN_DEPOSIT > value){
-            // 1. check if in challenge
-            // 2. check weather sender is submitter
-            // 2.1 submitter should confirm all batch then withdraw
-            if (msg.sender == submitter){
+            // 1. check weather sender is submitter
+            // 1.1 submitter should confirm all batch then withdraw
+            if (msg.sender == submitter && lastBatchSequenced != 0){
                 require(
-                    originTimestamps[lastBatchSequenced]+FINALIZATION_PERIOD_SECONDS <= block.timestamp,
+                    originTimestamps[lastBatchSequenced]+
+                        FINALIZATION_PERIOD_SECONDS <= block.timestamp,
                     "submitter should wait batch to be confirm"
                 );
             }
@@ -176,6 +210,7 @@ contract ZKEVM is CircuitConfig,OwnableUpgradeable {
         if (amount > value){
             withdrawAmount = value;
         }
+        deposits[msg.sender] -= amount;
         _transfer(msg.sender, withdrawAmount);
     }
 
@@ -192,15 +227,25 @@ contract ZKEVM is CircuitConfig,OwnableUpgradeable {
         return true;
     }
 
-    function isInChallenge(address user) public view returns (bool) {
-        if (user == submitter){
-            if (!challenges[challengeNum-1].finished){
-                return true;
-            }
+    function isStaked(address addr) public view returns (bool) {
+        return deposits[addr] != 0;
+    }
+
+    function isBatchInChallenge(uint64 batchIndex) public view returns (bool) {
+        return challenges[batchIndex].challenger != address(0) && !challenges[batchIndex].finished;
+    }
+
+    function isUserInChallenge(address user) public view returns (bool) {
+        if (lastBatchSequenced == 0){
+            return false;
         }
-        for(uint64 i=0;i<challengeNum;i++){
-            if(challenges[i].challenger == user && !challenges[i].finished){
-                return true;
+        if (user == submitter){
+                return !confirmBatchIndex[lastBatchSequenced-1];
+        }else {
+            for(uint64 i=0;i<lastBatchSequenced;i++){
+                if(challenges[i].challenger == user && !challenges[i].finished){
+                    return true;
+                }
             }
         }
         return false;
